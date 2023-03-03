0ccd706f21aa0a8a185be744738e9efa35419c1c
R-Tim <R-Tim@DESKTOP-PTDEOCU>
2023-03-03 23:58:16 +0100

    added cancel dialog

--- src/components/Tender.vue
+++ src/components/Tender.vue
@@ -411,7 +411,7 @@
     <v-row class="justify-end pb-15 mr-12 mt-3">
       <v-col cols="9" md="2">
         <v-btn type="submit" block class="mt-2" variant="outlined" color="blue"
-          size="large">
+          size="large" @click="canselDialog = true">
           Cancel
         </v-btn>
         </v-col>
@@ -423,6 +423,23 @@
       </v-col>
     </v-row>
   </v-container>
+
+  <div class="text-center">
+    <v-dialog v-model="canselDialog" activator="parent" width="500">
+      <v-card>
+        <v-toolbar color="primary" title="CANCELLATION CONFIRMATION" height="50"></v-toolbar>
+          <v-card-text class="text-center">
+            <div>Do you really want to cancel the new Tender creation?</div>
+            <div>All you entered data will be lost</div>
+          </v-card-text>
+          <v-row class="justify-center mb-8 mt-3">
+            <v-btn @click="canselDialog = false" width="160" class="mx-2">No</v-btn>
+            <v-btn color="primary" width="160" class="mx-2">Yes</v-btn>
+          </v-row>
+      </v-card>
+    </v-dialog>
+  </div>
+
 </template>
 
 <script>
@@ -460,6 +477,7 @@ export default {
     },
     valid: false,
     dialog: false,
+    canselDialog: false,
     isContract: false,
     isAwardDecision: false,
     isRejectedDecision: false,
