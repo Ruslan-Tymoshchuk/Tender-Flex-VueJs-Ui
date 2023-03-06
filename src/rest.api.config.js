eb259f42522a951840d2e870aa37e43898259e77
R-Tim <R-Tim@DESKTOP-PTDEOCU>
2023-03-06 12:59:40 +0100

    adjustments in api config

--- src/rest.api.config.js
+++ src/rest.api.config.js
@@ -1,6 +1,6 @@
 export const restApiConfig = {
   host:'http://localhost:8080/',
-  logIn:'api/v1/auth/login',
+  logIn:'api/v1/auth/signin',
   countries: 'api/v1/global/countries',
   tenderTypes: 'api/v1/global/tender-types',
   currencies: 'api/v1/global/currencies',
