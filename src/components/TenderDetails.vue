b034eb5bd10163f82122818769839f5588a90ec5
R-Tim <R-Tim@DESKTOP-PTDEOCU>
2023-03-14 22:31:09 +0100

    improved tender details page

--- src/components/TenderDetails.vue
+++ src/components/TenderDetails.vue
@@ -1,4 +1,34 @@
 <template>
+  <v-toolbar color="blue" extended extension-height="100">
+    <v-chip href="/contractor-module/tenders-list" style="margin-left: 12rem" variant="text" text-color="white"
+      prepend-icon="mdi-keyboard-backspace">Back
+    </v-chip>
+    <template v-slot:extension>
+      <v-container class="px-15">
+        <v-toolbar-title class="ml-10 mb-4" style="font-size: 2rem">Tender Details</v-toolbar-title>
+        <v-tabs v-model="tab" height="18" class="mb-15 ml-10" color="indigo-darken-2">
+          <v-tab value="offers">Offers</v-tab>
+          <v-tab value="tenderDescription">Tender Description</v-tab>
+        </v-tabs>
+      </v-container>
+    </template>
+  </v-toolbar>
+
+  <v-window v-model="tab" class="mt-n7 pb-10" >
+    <v-window-item value="offers">
+      <v-card class="mx-auto" elevation="8" max-width="1000">
+        <v-toolbar color="white" height="200">
+          <v-toolbar-title class="text-center" style="font-size: 2rem">“There are no received Offers”</v-toolbar-title>
+        </v-toolbar>
+      </v-card>
+    </v-window-item>
+
+    <v-window-item value="tenderDescription">
+      <v-card class="mx-auto" elevation="8" max-width="1000">
+        <v-toolbar color="white" height="200" class="text-left"></v-toolbar>
+      </v-card>
+    </v-window-item>
+  </v-window>
 </template>
 
 <script>
@@ -6,23 +36,11 @@ import { restApiConfig } from "@/rest.api.config"
 
 export default {
   data: () => ({
-    tenders: 0,
-    offers: 0,
+    tender: null,
+    tab: "tenderDescription",
   }),
 
   methods: {
-    getTendersAmountByContractor() {
-      fetch(`${restApiConfig.host}${restApiConfig.amountTendersByContractor}`, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          'Accept': 'application/json',
-        }
-      })
-        .then(response => response.json())
-        .then(tendersAmount => this.tenders = tendersAmount)
-    },
-
     getTenderBy(id) {
       fetch(`${restApiConfig.host}${restApiConfig.tenderDetails}/${id}`, {
         method: 'GET',
@@ -32,12 +50,11 @@ export default {
         }
       })
         .then(response => response.json())
-        .then(responseData => console.log(responseData))
+        .then(tenderDetailsResponse => this.tender = tenderDetailsResponse)
     }
   },
 
   mounted() {
-    this.getTendersAmountByContractor();
     this.getTenderBy(this.$route.params.id);
   }
 }
