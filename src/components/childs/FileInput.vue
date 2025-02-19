ccdc70bb7698c4d5c2e9c5a2332b3fe62580cb0e
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2025-02-19 14:07:17 +0100

    refactored select preview file

---
+++ src/components/childs/FileInput.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-row class="mt-5 mx-8">
+    <v-item v-if="!isFile">
+      <v-text-field
+      single-line
+      :label="label"
+      variant="outlined"
+      density="compact"
+      readonly
+      ></v-text-field>
+      <input
+        :id="fileInputId"
+        class="d-none"
+        type="file"
+        accept="application/pdf"
+        @change="selectFile"
+      ><v-btn
+        color="primary"
+        rounded="0"
+        height="40"
+        width="150"
+      ><label
+        class="file-label"
+        :for="fileInputId"
+      >Select</label
+      ><v-tooltip
+        activator="parent"
+        location="top"
+      >{{ hint }}</v-tooltip
+      ></v-btn>
+    </v-item>
+    <v-item v-else-if="isFile">
+      <v-chip
+        size="large"
+        class="mb-6"
+        closable
+        color="blue"
+        prepend-icon="mdi-file-document-multiple-outline"
+        label
+        @click:close="isFile = false"
+        @click="showFile"
+      ><div
+        id="text"
+        style="width: 50rem"
+      > {{ file.name }} </div>
+      </v-chip>
+    </v-item>
+  </v-row>
+
+  <FileViewerModal
+    :fileViewer="fileViewer">
+  </FileViewerModal>
+</template>
+
+<script>
+import FileViewerModal from "@/components/childs/FileViewerModal.vue";
+
+export default {
+  components: {
+    FileViewerModal
+  },
+
+  data: () => ({
+    isFile: false,
+    file: null,
+    fileViewer: {
+      isDialog: false,
+      documentUrl: '',
+    },
+  }),
+
+  props: {
+    label: '',
+    fileInputId: '',
+    hint: ''
+  },
+
+  methods: {
+    selectFile(event) {
+      this.isFile = true;
+      this.file = event.target.files[0];
+      this.$emit('selectFile', this.file)
+    },
+
+    showFile() {
+      this.fileViewer.documentUrl = URL.createObjectURL(this.file);
+      this.fileViewer.isDialog = true;
+      this.$emit('showFile');
+    }
+  },
+}
+</script>
