4b2b7faf92b6fd7dd79353029b2e348661dc9554
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2025-02-26 19:40:03 +0100

    adjusted ui in tenders table

--- src/components/childs/TableBody.vue
+++ src/components/childs/TableBody.vue
@@ -1,26 +1,26 @@
 <template>
   <v-table>
     <tbody>
-      <tr class="table" v-for="tender in tenders" :key="tender.id">
-        <td class="v-col-3 text-left">
-          <router-link :to="{ name: 'tender-details', params: { tenderId: tender.id } }" class="cpv-code" >
-            <div>{{ tender.cpv.code }}</div>
-          </router-link>
-          <strong>{{ tender.cpv.summary }}</strong>
-        </td>
-        <td class="v-col-2 text-center">{{ tender.companyProfile.officialName }}</td>
-        <td class="v-col-2 text-center">{{ TENDER_STATUS[tender.status] }}</td>
-        <td class="v-col-2 text-center">{{ tender.offerSubmissionDeadline }}</td>
-        <td class="v-col-2">
-          <div v-if="TENDER_STATUS[tender.status] === TENDER_STATUS.TENDER_IN_PROGRESS">
+      <tr v-for="tender in tenders" :key="tender.id">
+        <div :class="isDisabled(TENDER_STATUS[tender.status])">
+          <td class="v-col-4 text-left cpv-text">
+            <router-link :to="{ name: 'tender-details', params: { tenderId: tender.id } }" class="cpv-code">
+              <div>{{ tender.cpv.code }}</div>
+            </router-link>
+            <strong>{{ tender.cpv.summary }}</strong>
+          </td>
+          <td class="v-col-2 text-center">{{ tender.companyProfile.officialName }}</td>
+          <td class="v-col-2 text-center">{{ TENDER_STATUS[tender.status] }}</td>
+          <td class="v-col-2 text-center">{{ tender.offerSubmissionDeadline }}</td>
+          <td class="v-col-2">
             <div v-if="this.$route.params.role === USER_ROLE.CONTRACTOR" class="text-center">
               {{ tender.offersCount }}
             </div>
             <div v-else-if="this.$route.params.role === USER_ROLE.BIDDER" class="ml-7 text-left">
               {{ OFFER_STATUS[tender.offerStatus] }}
             </div>
-          </div>
-        </td>
+          </td>
+        </div>
       </tr>
     </tbody>
   </v-table>
@@ -38,6 +38,16 @@ export default {
   props: {
     tenders: null
   },
+
+  methods: {
+    isDisabled(status) {
+      if (status === TENDER_STATUS.TENDER_IN_PROGRESS) {
+        return "table-row";
+      } else {
+        return "table-row disabled";
+      }
+    }
+  }
 
 }
 </script>
