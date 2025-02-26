b92a65729a0198bb854d3bb43501b6d57daf57e1
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2025-02-26 21:45:41 +0100

    adjusted constants

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,26 +1,26 @@
 export const OFFER_STATUS = {
-  OFFER_HAS_NOT_SENT: 'Offer has not been sent',
-  OFFER_SENT_TO_CONTRACTOR: 'Offer sent',
-  OFFER_RECEIVED: 'Offer received',
-  OFFER_SELECTED: 'Offer selected',
-  OFFER_REJECTED_BY_CONTRACTOR: 'Offer rejected by contractor',
-  OFFER_REJECTED_BY_BIDDER: 'Offer rejected by bidder',
-  OFFER_SELECTED_BY_CONTRACTOR: 'Offer selected by contracror',
-  CONTRACT_DECLINED_BY_BIDDER: 'Offer declined by bidder',
-  CONTRACT_APPROVED_BY_BIDDER: 'Offer approved by bidder'
+  OFFER_HAS_NOT_SENT: "Offer hasn't sent",
+  OFFER_SENT_TO_CONTRACTOR: "Offer sent",
+  OFFER_RECEIVED: "Offer received",
+  OFFER_SELECTED: "Offer selected",
+  OFFER_REJECTED_BY_CONTRACTOR: "Offer rejected by contractor",
+  OFFER_REJECTED_BY_BIDDER: "Offer rejected by bidder",
+  OFFER_SELECTED_BY_CONTRACTOR: "Offer selected by contracror",
+  CONTRACT_DECLINED_BY_BIDDER: "Offer declined by bidder",
+  CONTRACT_APPROVED_BY_BIDDER: "Offer approved by bidder"
 }
 
 export const TENDER_STATUS = {
-  TENDER_IN_PROGRESS: 'Tender in progress',
-  TENDER_CLOSED: 'Tender closed'
+  TENDER_IN_PROGRESS: "Tender in progress",
+  TENDER_CLOSED: "Tender closed"
 }
 
 export const PROCEDURE = {
-  OPEN_PROCEDURE: 'Open procedure'
+  OPEN_PROCEDURE: "Open procedure"
 }
 
 export const LANGUAGE = {
-  ENGLISH: 'English'
+  ENGLISH: "English"
 }
 
 export const USER_ROLE = {
