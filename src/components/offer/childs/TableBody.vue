5b3bd525bdf81c7776accc6d0250315d6827e856
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2025-03-06 23:01:13 +0100

    refactored offer list

--- src/components/offer/childs/TableBody.vue
+++ src/components/offer/childs/TableBody.vue
@@ -1,16 +1,15 @@
 <template>
   <v-sheet v-for="offer in offers" :key="offer.id">
-    <v-sheet class="table-row">
-      <td class="v-col-2 text-left cpv-text">
-        <router-link class="cpv-code" :to="{ name: 'offer-details', params: { offerId: offer.id } }">
-          <strong>{{ offer.companyProfile.officialName }}</strong>
-        </router-link>
-      </td>
-      <td class="v-col-3 text-left">{{ offer.tender.cpv.summary }}</td>
-      <td class="v-col-2 text-center">{{ `${offer.currency.code}.${offer.bidPrice}` }}</td>
-      <td class="v-col-1 text-center">{{ offer.companyProfile.country.name }}</td>
-      <td class="v-col-2 text-center">{{ offer.publication }}</td>
-      <td class="v-col-1 ml-6 text-left"> {{ OFFER_STATUS[offer.status] }} </td>
+    <v-sheet class="table-row-offer">
+      <router-link class="cpv-code" :to="{ name: 'offer-details', params: { offerId: offer.id } }"
+        style="width: 14rem; margin-left: 1rem;">
+        <strong>{{ offer.companyProfile.officialName }}</strong>
+      </router-link>
+      <td style="width: 20rem;">{{ offer.tender.cpv.summary }}</td>
+      <td style="width: 10rem;">{{ `${offer.currency.code}.${offer.bidPrice}` }}</td>
+      <td style="width: 10rem;">{{ offer.companyProfile.country.name }}</td>
+      <td style="width: 10rem;">{{ offer.publication }}</td>
+      <td style="width: 10rem;"> {{ OFFER_STATUS[offer.status] }} </td>
     </v-sheet>
   </v-sheet>
 </template>
